099f6435342b043f006f110027811cdb05a98fca
Gaurav Rehan <gaurav.rehan@gmail.com>
2017-03-11 23:04:17 +0530

    BST Interface defined

--- src/main/java/com/home/datastructures/tree/Position.java
+++
@@ -1,5 +0,0 @@
-package com.home.datastructures.tree;
-
-public interface Position<E> {
-	E getElement() throws IllegalStateException;
-}
---
+++ src/main/java/com/home/datastructures/trees/BinarySearchTree.java
@@ -0,0 +1,17 @@
+package com.home.datastructures.trees;
+
+/**
+ * Created by richa on 3/11/2017.
+ */
+public interface BinarySearchTree {
+
+    //Traversal - Inorder, PreOrder, and PostOrder
+    //Recursive Search
+    //Iterative Search
+    //Tree Minimum
+    //Tree Maximum
+    //Tree Successor
+    //Tree Predecessor
+    //Tree Insert
+    //Tree Deletion
+}
